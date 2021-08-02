c3b3b42e9e70fe633da0584646fbd2e96e3b6088
eduardoaraya <eduardojezine@gmail.com>
2021-08-01 23:39:46 -0300

    remove db file

--- src/src/app/bucket/bucket.controller.ts
+++ src/src/app/bucket/bucket.controller.ts
@@ -1,17 +1,51 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import { Response, Request } from 'express';
 import { BucketService } from './bucket.service';
+
+enum BuckerMessageErrosEnum {
+  E_VALIDATE_FAILURE = 'data_validation_error',
+  INTERNAL_SERVER_ERROR = 'internal_error',
+}
+
+const BuckerMessageErros = {
+  [BuckerMessageErrosEnum.E_VALIDATE_FAILURE]: {
+    message: "O campo 'data' obrigatório",
+    code: 'E_VALIDATE_FAILURE',
+  },
+  [BuckerMessageErrosEnum.INTERNAL_SERVER_ERROR]: {
+    message: 'Erro interno, tente novamente mais tarde',
+    code: 'INTERNAL_SERVER_ERROR',
+  },
+};
 
 @Controller('bucket')
 export class BucketController {
   public constructor(private bucketService: BucketService) {}
 
-  @Get('create')
-  public async create(@Res() res: Response) {
+  getError(type: BuckerMessageErrosEnum) {
+    return BuckerMessageErros[type];
+  }
+
+  @Post('create')
+  public async create(@Req() req: Request, @Res() res: Response) {
     try {
+      let { data } = req.body;
+      if (!data) {
+        const { code, message } = this.getError(
+          BuckerMessageErrosEnum.E_VALIDATE_FAILURE,
+        );
+        return res.status(HttpStatus.BAD_REQUEST).json({
+          code,
+          message,
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+      }
+      if (data instanceof Object) {
+        data = JSON.stringify(data);
+      }
       return res.status(HttpStatus.ACCEPTED).json({
         success: true,
-        data: await this.bucketService.create('teste'),
+        data: await this.bucketService.create(data),
       });
     } catch (Error) {
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
--- src/src/app/crypt/crypt.service.ts
+++ src/src/app/crypt/crypt.service.ts
@@ -2,13 +2,19 @@ import * as CryptoJS from 'crypto-js';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
+export interface CryptServiceInterface {
+  encrypt(data: string): Promise<string>;
+  decrypt(data: string): Promise<string>;
+}
+
 @Injectable()
-export class CryptService {
+export class CryptService implements CryptServiceInterface {
   private KEY: string;
 
   constructor(private configService: ConfigService) {
     this.KEY = this.configService.get('CRYPT_SECRET_KEY');
   }
+
   async encrypt(data: string): Promise<string> {
     return CryptoJS.Rabbit.encrypt(data, this.KEY).toString();
   }
